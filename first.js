2fffc8a1363343c18055f69c3f01fb5b1a6f9d2b
Serumola <pakoserumola02@gmail.com>
2026-04-20 10:23:02 +0200

    first commit

---
+++ first.js
@@ -0,0 +1,42 @@
+let cows = 9 ;
+let name = "h";
+let gpa = 3.7;
+let numbers = [3,6,9,12,15]; //this is an array - colection of the same type, this is one dimentional. arrays are 0 indexed
+
+//function - block of code which perform a specific task
+function operator (){
+    console.log(my)
+
+}
+function f(x){
+    console.log(x+1)
+}
+console.log(numbers[2])
+
+const add = (X,Y) => {
+    return
+}
+console.log(5 == 5)
+
+let digits = [1,2,3,4,5,6,7,8,9,10];
+let anotherDigits = digits.filter((number)=> number % 2===0);
+console.log(anotherDigits);
+
+let digts = [1,2,3,4,5,6,7,8,9,10];
+let anotherDigts = digts.reduce((acc,number)=> acc + number ,0 );
+console.log(anotherDigts);
+
+//splice
+let nums = [1,2,3,4,5,6,7,8,9,10];
+let nums2 = nums.splice(0,5);
+console.log(nums2);
+
+
+//slice
+let nums3 = nums.slice(0,5);
+console.log(nums3);
+
+
+
+
+
